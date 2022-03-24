6171cc8dea32f569852c76ae533c1f3de8231c47
yml2213 <A2213290201@gmail.com>
2022-03-24 14:05:34 +0800

    签到任务 进行双平台脚本测试,青龙加圈x 有bug@我

--- xmly/xmly.js
+++ xmly/xmly.js
@@ -15,7 +15,7 @@ export yml_xmly_data='cookie1@cookie2'
  多账号使用 @ 分割；
 (给小白啰嗦一句:export XXX ==> 是青龙 "配置文件" 变量格式; 如果要在 "环境变量" 中添加,不需要export)
 =============青龙变量实例=============
-export yml_xmly_cookie='Cookie: domain=.ximalaya.com; path=/; channel=ios-b1; 1&_device=iPhone&32866292-70F5-45A8-9F03-33A3DDEA3A94&9.0.22; impl=com.gemd.iting; XUM=32866292-70F5-45A8-9F03-33A3DDEA3A94; c-oper=%E8%81%94%E9%80%9A; net-mode=WIFI; res=1170%2C2532; 1&_token=230652218&497C23E0340C08D51383BE1B784794F0BC6D50F82B09F52FEA8D4A73F13AD896AA97DB615728147M825F3393CED220B_; idfa=32866292-70F5-45A8-9F03-33A3DDEA3A94; device_model=iPhone%2013%20Pro; XD=wrbjHmU5xnR+9Nz5Tx/zPg2yNxJSLDRQKe9VGyFxirbG6aQ5HmxbVGs0Mg17Xff92KG0ARPtymt8WsOejmP5VQ==; fp=009317647e2322q22164v05b2500000020211100200000001200001000; freeFlowType=0; minorProtectionStatus=0'
+export yml_xmly_cookie='domain=.ximalaya.com; path=/; channel=ios-b1; 1&_device=iPhone&32866292-70F5-45A8-9F03-33A3DDEA3A94&9.0.22; impl=com.gemd.iting; XUM=32866292-70F5-45A8-9F03-33A3DDEA3A94; c-oper=%E8%81%94%E9%80%9A; net-mode=WIFI; res=1170%2C2532; 1&_token=230652218&497C23E0340C08D51383BE1B784794F0BC6D50F82B09F52FEA8D4A73F13AD896AA97DB615728147M825F3393CED220B_; idfa=32866292-70F5-45A8-9F03-33A3DDEA3A94; device_model=iPhone%2013%20Pro; XD=wrbjHmU5xnR+9Nz5Tx/zPg2yNxJSLDRQKe9VGyFxirbG6aQ5HmxbVGs0Mg17Xff92KG0ARPtymt8WsOejmP5VQ==; fp=009317647e2322q22164v05b2500000020211100200000001200001000; freeFlowType=0; minorProtectionStatus=0'
 =============变量解释==========
 只需要自己抓包一个 cookie 即可
 =============变量获取==========
