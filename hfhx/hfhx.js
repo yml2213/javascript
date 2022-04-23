1e23e129bf03b99640f0adbce34da99a4b9efb72
yml2213 <a2213290201@gmail.com>
2022-04-23 12:14:38 +0800

    4-23  增加签到  查询积分  通知 功能

---
+++ hfhx/hfhx.js
@@ -0,0 +1,323 @@
+/**
+ * 汇丰汇选 
+ * cron 10 7 * * *  yml2213_javascript_master/hfhx.js
+ * 
+ * 汇丰汇选 app  
+ * 4-23  增加签到  查询积分  通知 功能
+ * 
+ * 
+ * 感谢群友提供
+ * 感谢所有测试人员
+ * ========= 青龙 =========
+ * 变量格式: export hfhx_data=' X-HSBC-E2E-Trust-Token1 @ X-HSBC-E2E-Trust-Token2 '   多个账号用 @分割 
+ * 
+ * 抓包： 先打开 app - 发现 - 右上角 '领积分' , 然后再打开抓包软件  , 抓签到包  , 找到有 X-HSBC-E2E-Trust-Token 的包就行了
+ * 
+ * 还是不会的请百度或者群里求助: tg: https://t.me/yml_tg  通知: https://t.me/yml2213_tg
+ */
+
+const $ = new Env("汇丰汇选");
+const notify = $.isNode() ? require('./sendNotify') : '';
+const Notify = 1; //0为关闭通知，1为打开通知,默认为1
+const debug = 0; //0为关闭调试，1为打开调试,默认为0
+//////////////////////
+let hfhx_dataArr = [];
+let hfhx_data = process.env.hfhx_data;
+let msg = '';
+
+
+!(async () => {
+
+	if (!(await Envs()))  //多账号分割 判断变量是否为空  初步处理多账号
+		return;
+	else {
+
+		console.log(`\n本地脚本4-19`);
+
+		//  console.log(`\n 脚本已恢复正常状态,请及时更新! `);
+		console.log(`\n 脚本测试中,有bug及时反馈! \n`);
+		console.log(`\n 脚本测试中,有bug及时反馈! \n`);
+		console.log(`\n 脚本测试中,有bug及时反馈! \n`);
+
+		console.log(`\n================================================\n脚本执行 - 北京时间(UTC+8): ${new Date(
+			new Date().getTime() + new Date().getTimezoneOffset() * 60 * 1000 + 8 * 60 * 60 * 1000).toLocaleString()} \n================================================\n`);
+
+		await wyy();
+
+
+		console.log(`\n=================== 共找到 ${hfhx_dataArr.length} 个账号 ===================`)
+		if (debug) {
+			console.log(`【debug】 这是你的账号数组:\n ${hfhx_dataArr}`);
+		}
+
+
+		for (let index = 0; index < hfhx_dataArr.length; index++) {
+
+
+			let num = index + 1
+			console.log(`\n========= 开始【第 ${num} 个账号】=========\n`)
+
+			data = hfhx_dataArr[index].split('&');
+			if (debug) {
+				console.log(`\n 【debug】 这是你第 ${num} 账号信息:\n ${data}\n`);
+			}
+
+
+
+			console.log('开始 查询积分余额');
+			await user_info();
+			await $.wait(2 * 1000);
+
+
+			await SendMsg(msg);
+
+
+		}
+
+
+	}
+
+})()
+	.catch((e) => $.logErr(e))
+	.finally(() => $.done())
+
+
+
+
+
+
+
+
+/**
+ * 查询积分余额  get
+ * https://m.prod.app.hsbcfts.com.cn/api/sapp/biz/pointscenter/pointsindex/v1
+ */
+function user_info(timeout = 3 * 1000) {
+
+	return new Promise((resolve) => {
+		let url = {
+			url: `https://m.prod.app.hsbcfts.com.cn/api/sapp/biz/pointscenter/pointsindex/v1`,
+			headers: {
+
+				'Connection': 'keep-alive',
+				'Host': 'm.prod.app.hsbcfts.com.cn',
+				'Referer': 'https://m.prod.app.hsbcfts.com.cn/activities/points/',
+				'X-HSBC-Global-Channel-Id': 'MOBILE',
+				'X-HSBC-E2E-Trust-Token': data
+
+			},
+		}
+
+		if (debug) {
+			console.log(`\n 【debug】=============== 这是 查询积分余额 请求 url ===============`);
+			console.log(url);
+		}
+		$.get(url, async (error, response, data) => {
+			try {
+				if (debug) {
+					console.log(`\n\n 【debug】===============这是 查询积分余额 返回data==============`);
+					console.log(data)
+					console.log(`======`)
+					console.log(JSON.parse(data))
+				}
+				let result = JSON.parse(data);
+				if (result.retCode == 10000) {
+
+					console.log(`\n 查询积分余额 成功 🎉 \n你现在有 ${result.data.pointBalance} 积分 , 您已经连续签到  ${result.data.consecutiveSignedInDays} 天 , 您本月已签到  ${result.data.signInDaysOfMonth} 天\n 您今天签到状态: ${result.data.todaySignInStatus}\n`);
+					msg += `\n 查询积分余额 成功 🎉 \n你现在有 ${result.data.pointBalance} 积分 , 您已经连续签到  ${result.data.consecutiveSignedInDays} 天 , 您本月已签到  ${result.data.signInDaysOfMonth} 天\n 您今天签到状态: ${result.data.todaySignInStatus}\n`
+
+					if (result.data.todaySignInStatus != true) {
+						console.log(`\n您今天还没签到,去签到喽!`);
+						await signIn();
+						await $.wait(2 * 1000);
+					}
+
+
+				} else {
+
+					console.log(`\n 查询积分余额:  失败 ❌ 了呢,原因未知！\n ${data} \n `)
+
+				}
+
+			} catch (e) {
+				console.log(e)
+			} finally {
+				resolve();
+			}
+		}, timeout)
+	})
+}
+
+
+/**
+ * 签到  post
+ * https://m.prod.app.hsbcfts.com.cn/api/sapp/biz/pointscenter/signin/v1
+ */
+function signIn(timeout = 3 * 1000) {
+
+	return new Promise((resolve) => {
+		let url = {
+			url: `https://m.prod.app.hsbcfts.com.cn/api/sapp/biz/pointscenter/signin/v1`,
+			headers: {
+
+				'Origin': 'https://m.prod.app.hsbcfts.com.cn',
+				'Content-Type': 'application/json',
+				'X-HSBC-E2E-Trust-Token': data,
+				'Host': 'm.prod.app.hsbcfts.com.cn',
+				'X-HSBC-Request-Correlation-Id': 'e18e39c3-5f15-4aae-bd2a-8c21e340986a',
+				'Referer': 'https://m.prod.app.hsbcfts.com.cn/activities/points/',
+				'X-HSBC-Global-Channel-Id': 'MOBILE',
+				'Connection': 'keep-alive'
+			},
+			body: '',
+		}
+
+		if (debug) {
+			console.log(`\n 【debug】=============== 这是 签到 请求 url ===============`);
+			console.log(url);
+		}
+		$.post(url, async (error, response, data) => {
+			try {
+				if (debug) {
+					console.log(`\n\n 【debug】===============这是 签到 返回data==============`);
+					console.log(data)
+					console.log(`======`)
+					console.log(JSON.parse(data))
+				}
+				let result = JSON.parse(data);
+				if (result.retCode == 10000) {
+					console.log(`\n 签到: ${result.message} 🎉  获得积分 ${result.data.pointAmount} 个\n`);
+
+				} else if (result.retCode == 2003) {
+					console.log(`\n 签到: ${result.message} \n`);
+
+				} else {
+
+					console.log(`\n 签到:  失败 ❌ 了呢,原因未知！\n ${data} \n `)
+
+				}
+
+			} catch (e) {
+				console.log(e)
+			} finally {
+				resolve();
+			}
+		}, timeout)
+	})
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#region 固定代码
+// ============================================变量检查============================================ \\
+async function Envs() {
+	if (hfhx_data) {
+		if (hfhx_data.indexOf("@") != -1) {
+			hfhx_data.split("@").forEach((item) => {
+				hfhx_dataArr.push(item);
+			});
+		} else {
+			hfhx_dataArr.push(hfhx_data);
+		}
+	} else {
+		console.log(`\n 【${$.name}】：未填写变量 hfhx_data`)
+		return;
+	}
+
+	return true;
+}
+
+// ============================================发送消息============================================ \\
+async function SendMsg(message) {
+	if (!message)
+		return;
+
+	if (Notify > 0) {
+		if ($.isNode()) {
+			var notify = require('./sendNotify');
+			await notify.sendNotify($.name, message);
+		} else {
+			$.msg(message);
+		}
+	} else {
+		console.log(message);
+	}
+}
+
+/**
+ * 随机数生成
+ */
+function randomString(e) {
+	e = e || 32;
+	var t = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890",
+		a = t.length,
+		n = "";
+	for (i = 0; i < e; i++)
+		n += t.charAt(Math.floor(Math.random() * a));
+	return n
+}
+
+/**
+ * 随机整数生成
+ */
+function randomInt(min, max) {
+	return Math.round(Math.random() * (max - min) + min)
+}
+
+//每日网抑云
+function wyy(timeout = 3 * 1000) {
+	return new Promise((resolve) => {
+		let url = {
+			url: `https://keai.icu/apiwyy/api`
+		}
+		$.get(url, async (err, resp, data) => {
+			try {
+				data = JSON.parse(data)
+				console.log(`\n 【网抑云时间】: ${data.content}  by--${data.music}`);
+
+			} catch (e) {
+				$.logErr(e, resp);
+			} finally {
+				resolve()
+			}
+		}, timeout)
+	})
+}
+
+//#endregion
+
+
+
+
+// prettier-ignore
+function Env(t, e) { "undefined" != typeof process && JSON.stringify(process.env).indexOf("GITHUB") > -1 && process.exit(0); class s { constructor(t) { this.env = t } send(t, e = "GET") { t = "string" == typeof t ? { url: t } : t; let s = this.get; return "POST" === e && (s = this.post), new Promise((e, i) => { s.call(this, t, (t, s, r) => { t ? i(t) : e(s) }) }) } get(t) { return this.send.call(this.env, t) } post(t) { return this.send.call(this.env, t, "POST") } } return new class { constructor(t, e) { this.name = t, this.http = new s(this), this.data = null, this.dataFile = "box.dat", this.logs = [], this.isMute = !1, this.isNeedRewrite = !1, this.logSeparator = "\n", this.startTime = (new Date).getTime(), Object.assign(this, e), this.log("", `🔔${this.name}, 开始!`) } isNode() { return "undefined" != typeof module && !!module.exports } isQuanX() { return "undefined" != typeof $task } isSurge() { return "undefined" != typeof $httpClient && "undefined" == typeof $loon } isLoon() { return "undefined" != typeof $loon } toObj(t, e = null) { try { return JSON.parse(t) } catch { return e } } toStr(t, e = null) { try { return JSON.stringify(t) } catch { return e } } getjson(t, e) { let s = e; const i = this.getdata(t); if (i) try { s = JSON.parse(this.getdata(t)) } catch { } return s } setjson(t, e) { try { return this.setdata(JSON.stringify(t), e) } catch { return !1 } } getScript(t) { return new Promise(e => { this.get({ url: t }, (t, s, i) => e(i)) }) } runScript(t, e) { return new Promise(s => { let i = this.getdata("@chavy_boxjs_userCfgs.httpapi"); i = i ? i.replace(/\n/g, "").trim() : i; let r = this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout"); r = r ? 1 * r : 20, r = e && e.timeout ? e.timeout : r; const [o, h] = i.split("@"), n = { url: `http://${h}/v1/scripting/evaluate`, body: { script_text: t, mock_type: "cron", timeout: r }, headers: { "X-Key": o, Accept: "*/*" } }; this.post(n, (t, e, i) => s(i)) }).catch(t => this.logErr(t)) } loaddata() { if (!this.isNode()) return {}; { this.fs = this.fs ? this.fs : require("fs"), this.path = this.path ? this.path : require("path"); const t = this.path.resolve(this.dataFile), e = this.path.resolve(process.cwd(), this.dataFile), s = this.fs.existsSync(t), i = !s && this.fs.existsSync(e); if (!s && !i) return {}; { const i = s ? t : e; try { return JSON.parse(this.fs.readFileSync(i)) } catch (t) { return {} } } } } writedata() { if (this.isNode()) { this.fs = this.fs ? this.fs : require("fs"), this.path = this.path ? this.path : require("path"); const t = this.path.resolve(this.dataFile), e = this.path.resolve(process.cwd(), this.dataFile), s = this.fs.existsSync(t), i = !s && this.fs.existsSync(e), r = JSON.stringify(this.data); s ? this.fs.writeFileSync(t, r) : i ? this.fs.writeFileSync(e, r) : this.fs.writeFileSync(t, r) } } lodash_get(t, e, s) { const i = e.replace(/\[(\d+)\]/g, ".$1").split("."); let r = t; for (const t of i) if (r = Object(r)[t], void 0 === r) return s; return r } lodash_set(t, e, s) { return Object(t) !== t ? t : (Array.isArray(e) || (e = e.toString().match(/[^.[\]]+/g) || []), e.slice(0, -1).reduce((t, s, i) => Object(t[s]) === t[s] ? t[s] : t[s] = Math.abs(e[i + 1]) >> 0 == +e[i + 1] ? [] : {}, t)[e[e.length - 1]] = s, t) } getdata(t) { let e = this.getval(t); if (/^@/.test(t)) { const [, s, i] = /^@(.*?)\.(.*?)$/.exec(t), r = s ? this.getval(s) : ""; if (r) try { const t = JSON.parse(r); e = t ? this.lodash_get(t, i, "") : e } catch (t) { e = "" } } return e } setdata(t, e) { let s = !1; if (/^@/.test(e)) { const [, i, r] = /^@(.*?)\.(.*?)$/.exec(e), o = this.getval(i), h = i ? "null" === o ? null : o || "{}" : "{}"; try { const e = JSON.parse(h); this.lodash_set(e, r, t), s = this.setval(JSON.stringify(e), i) } catch (e) { const o = {}; this.lodash_set(o, r, t), s = this.setval(JSON.stringify(o), i) } } else s = this.setval(t, e); return s } getval(t) { return this.isSurge() || this.isLoon() ? $persistentStore.read(t) : this.isQuanX() ? $prefs.valueForKey(t) : this.isNode() ? (this.data = this.loaddata(), this.data[t]) : this.data && this.data[t] || null } setval(t, e) { return this.isSurge() || this.isLoon() ? $persistentStore.write(t, e) : this.isQuanX() ? $prefs.setValueForKey(t, e) : this.isNode() ? (this.data = this.loaddata(), this.data[e] = t, this.writedata(), !0) : this.data && this.data[e] || null } initGotEnv(t) { this.got = this.got ? this.got : require("got"), this.cktough = this.cktough ? this.cktough : require("tough-cookie"), this.ckjar = this.ckjar ? this.ckjar : new this.cktough.CookieJar, t && (t.headers = t.headers ? t.headers : {}, void 0 === t.headers.Cookie && void 0 === t.cookieJar && (t.cookieJar = this.ckjar)) } get(t, e = (() => { })) { t.headers && (delete t.headers["Content-Type"], delete t.headers["Content-Length"]), this.isSurge() || this.isLoon() ? (this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, { "X-Surge-Skip-Scripting": !1 })), $httpClient.get(t, (t, s, i) => { !t && s && (s.body = i, s.statusCode = s.status), e(t, s, i) })) : this.isQuanX() ? (this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, { hints: !1 })), $task.fetch(t).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => e(t))) : this.isNode() && (this.initGotEnv(t), this.got(t).on("redirect", (t, e) => { try { if (t.headers["set-cookie"]) { const s = t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString(); s && this.ckjar.setCookieSync(s, null), e.cookieJar = this.ckjar } } catch (t) { this.logErr(t) } }).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => { const { message: s, response: i } = t; e(s, i, i && i.body) })) } post(t, e = (() => { })) { if (t.body && t.headers && !t.headers["Content-Type"] && (t.headers["Content-Type"] = "application/x-www-form-urlencoded"), t.headers && delete t.headers["Content-Length"], this.isSurge() || this.isLoon()) this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, { "X-Surge-Skip-Scripting": !1 })), $httpClient.post(t, (t, s, i) => { !t && s && (s.body = i, s.statusCode = s.status), e(t, s, i) }); else if (this.isQuanX()) t.method = "POST", this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, { hints: !1 })), $task.fetch(t).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => e(t)); else if (this.isNode()) { this.initGotEnv(t); const { url: s, ...i } = t; this.got.post(s, i).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => { const { message: s, response: i } = t; e(s, i, i && i.body) }) } } time(t, e = null) { const s = e ? new Date(e) : new Date; let i = { "M+": s.getMonth() + 1, "d+": s.getDate(), "H+": s.getHours(), "m+": s.getMinutes(), "s+": s.getSeconds(), "q+": Math.floor((s.getMonth() + 3) / 3), S: s.getMilliseconds() }; /(y+)/.test(t) && (t = t.replace(RegExp.$1, (s.getFullYear() + "").substr(4 - RegExp.$1.length))); for (let e in i) new RegExp("(" + e + ")").test(t) && (t = t.replace(RegExp.$1, 1 == RegExp.$1.length ? i[e] : ("00" + i[e]).substr(("" + i[e]).length))); return t } msg(e = t, s = "", i = "", r) { const o = t => { if (!t) return t; if ("string" == typeof t) return this.isLoon() ? t : this.isQuanX() ? { "open-url": t } : this.isSurge() ? { url: t } : void 0; if ("object" == typeof t) { if (this.isLoon()) { let e = t.openUrl || t.url || t["open-url"], s = t.mediaUrl || t["media-url"]; return { openUrl: e, mediaUrl: s } } if (this.isQuanX()) { let e = t["open-url"] || t.url || t.openUrl, s = t["media-url"] || t.mediaUrl; return { "open-url": e, "media-url": s } } if (this.isSurge()) { let e = t.url || t.openUrl || t["open-url"]; return { url: e } } } }; if (this.isMute || (this.isSurge() || this.isLoon() ? $notification.post(e, s, i, o(r)) : this.isQuanX() && $notify(e, s, i, o(r))), !this.isMuteLog) { let t = ["", "==============📣系统通知📣=============="]; t.push(e), s && t.push(s), i && t.push(i), console.log(t.join("\n")), this.logs = this.logs.concat(t) } } log(...t) { t.length > 0 && (this.logs = [...this.logs, ...t]), console.log(t.join(this.logSeparator)) } logErr(t, e) { const s = !this.isSurge() && !this.isQuanX() && !this.isLoon(); s ? this.log("", `❗️${this.name}, 错误!`, t.stack) : this.log("", `❗️${this.name}, 错误!`, t) } wait(t) { return new Promise(e => setTimeout(e, t)) } done(t = {}) { const e = (new Date).getTime(), s = (e - this.startTime) / 1e3; this.log("", `🔔${this.name}, 结束! 🕛 ${s} 秒`), this.log(), (this.isSurge() || this.isQuanX() || this.isLoon()) && $done(t) } }(t, e) }
