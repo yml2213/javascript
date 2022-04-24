142bc01e0996e2b11945fedb3d0e456ac4bae519
yml <2213290201@qq.com>
2022-04-24 09:34:23 +0800

    修复礼盒

--- ydcd/ydcd.js
+++ ydcd/ydcd.js
@@ -17,7 +17,7 @@
 const $ = new Env("有道词典");
 const notify = $.isNode() ? require('./sendNotify') : '';
 const Notify = 1; //0为关闭通知，1为打开通知,默认为1
-const debug = 1; //0为关闭调试，1为打开调试,默认为0
+const debug = 0; //0为关闭调试，1为打开调试,默认为0
 //////////////////////
 let ydcd_dataArr = [];
 let ydcd_data = process.env.ydcd_data;
