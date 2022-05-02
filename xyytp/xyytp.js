18ce047836d2f4cfe2da4cc9fa86408832cdc4b4
yml2213 <a2213290201@gmail.com>
2022-05-02 15:35:29 +0800

    5-2 完成 签到 任务

--- xyytp/xyytp.js
+++ xyytp/xyytp.js
@@ -21,7 +21,7 @@
 const $ = new Env("咸鱼要躺平");
 const notify = $.isNode() ? require("./sendNotify") : "";
 const Notify = 1; 		//0为关闭通知，1为打开通知,默认为1
-const debug = 1; 		//0为关闭调试，1为打开调试,默认为0
+const debug = 0; 		//0为关闭调试，1为打开调试,默认为0
 //////////////////////
 let ckStr = process.env.xyytp_data;
 let msg = "";
