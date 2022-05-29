f40cbaa564bb8852a430db59f54106967b17664f
yml2213 <a2213290201@gmail.com>
2022-05-29 23:16:51 +0800

    修复分红bug ,优化运行逻辑

--- tlm/tlm.js
+++ tlm/tlm.js
@@ -30,7 +30,7 @@
 const $ = new Env("推了吗");
 const notify = $.isNode() ? require("./sendNotify") : "";
 const Notify = 1 		//0为关闭通知,1为打开通知,默认为1
-const debug = 1 		//0为关闭调试,1为打开调试,默认为0
+const debug = 0 		//0为关闭调试,1为打开调试,默认为0
 ///////////////////////////////////////////////////////////////////
 let ckStr = process.env.tlm_data;
 let msg = "";
