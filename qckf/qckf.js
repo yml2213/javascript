789815c0a3e23b6f98f042542644db89c794bf1d
yml2213 <a2213290201@gmail.com>
2022-05-31 13:07:35 +0800

    增加版本号,修复领取失败bug

--- qckf/qckf.js
+++ qckf/qckf.js
@@ -9,7 +9,7 @@
  *
  * 5-29     完成 签到 自己培育 给好友培育 分享 等任务
  * 5-30     修复分享 邀请爱豆 领取失败bug
- * 5-31     增加版本号
+ * 5-31     增加版本号,修复领取失败bug
  *
  * 感谢所有测试人员
  * ========= 青龙--配置文件 =========
@@ -36,18 +36,18 @@ let ck_status = 0;
 let host = "coffeefarm.shheywow.com";
 let hostname = "https://" + host;
 ///////////////////////////////////////////////////////////////////
-let VersionCheck = "0.0.2"
-let Change = '修复异常停止,测试中!'
+let VersionCheck = "0.1.3"
+let Change = '增加版本号,修复领取失败bug!'
 let thank = `\n感谢 xx 的投稿`
 ///////////////////////////////////////////////////////////////////
 
 async function tips(ckArr) {
     let Version_latest = await Version_Check('qckf');
-    let Version = `\n📌 本地脚本: V_0.0.2  远程仓库脚本: V_${Version_latest}`
+    let Version = `\n📌 本地脚本: V_0.1.3  远程仓库脚本: V_${Version_latest}`
     console.log(`${Version}`);
-    msg += `${Version}`
+    msg += `\n${Version}`
     console.log(`📌 🆙 更新内容: ${Change}\n`);
-    msg += `${Change}`
+    msg += `\n${Change}`
 
     // console.log(thank);
     // msg += `${thank}`
@@ -78,20 +78,14 @@ async function start() {
 
     console.log("\n开始 更新token");
     await Update_token();
-    await $.wait(3 * 1000);
 
     if (ck_status == 0) {
         console.log("\n开始 任务列表");
         await Task_List();
-        await $.wait(3 * 1000);
+
 
         console.log("\n开始 用户信息");
         await user_info();
-        await $.wait(3 * 1000);
-
-
-        // await invitation_Reward();
-        // await $.wait(3 * 1000);
 
     }
 }
@@ -119,8 +113,7 @@ async function Update_token() {
         console.log(`    更新token: 成功 🎉`);
         msg += `\n     更新token: 成功 🎉`;
         _tokne = result.data.token;
-        // console.log(_tokne);
-        // return tokne;
+        await wait(3);
     } else {
         console.log(`    更新token: 失败 ❌ 了呢,原因未知!`);
         console.log(result);
@@ -149,10 +142,9 @@ async function Task_List() {
 
     if (result.error_code === 0) {
         if (result.data.sign.status === 0) {
-            console.log(`    签到状态: 今日还没签到 ,去签到喽 ,顺便分享下!🏃🏃🏃`);
-            msg += `\n    签到状态: 今日还没签到 ,去签到喽 ,顺便分享下!🏃🏃🏃`;
+            console.log(`    签到状态: 今日还没签到 ,去签到喽!🏃🏃🏃`);
+            msg += `\n    签到状态: 今日还没签到 ,去签到喽!🏃🏃🏃`;
             await SignIn();
-            await $.wait(3 * 1000);
         } else {
             console.log(`    签到状态: 今日签到过了 ,明天再来吧!`);
             msg += `\n    签到状态: 今日签到过了 ,明天再来吧!`;
@@ -161,7 +153,6 @@ async function Task_List() {
             console.log(`    每日分享状态: 每日分享未完成 ,去分享喽 !🏃🏃🏃`);
             msg += `\n    每日分享状态: 每日分享未完成 ,去分享喽 !🏃🏃🏃`;
             await share();
-            await $.wait(3 * 1000);
             await share_Reward();
         } else {
             console.log(`    每日分享状态: 今日每日分享过了 ,明天再来吧!`);
@@ -171,7 +162,6 @@ async function Task_List() {
             console.log(`    邀请好友: 可以领取爱豆 ${result.data.invitation.credit}`);
             msg += `\n    邀请好友: 可以领取爱豆 ${result.data.invitation.credit}`;
             await invitation_Reward();
-            await $.wait(3 * 1000);
         } else {
             console.log(`    邀请好友: 暂无可领取爱豆!`);
             msg += `\n    邀请好友: 暂无可领取爱豆!`;
@@ -182,7 +172,6 @@ async function Task_List() {
             msg += `\n    好友培育: ${result.data.foster.num}/3`
             if (result.data.foster.num < 3) {
                 await friend_breed_info();
-                await $.wait(3 * 1000);
             }
         } else {
             console.log(`    好友培育: 今日完成了 ,明天再来吧!`);
@@ -216,6 +205,7 @@ async function SignIn() {
     if (result.error_code === 0) {
         console.log(`    签到: 成功 ,获得 ${result.data.list[0].credit} 爱豆`);
         msg += `\n    签到: 成功 ,获得 ${result.data.list[0].credit} 爱豆`;
+        await wait(3);
     } else {
         console.log(`    签到: 失败 ❌ 了呢,原因未知!`);
         console.log(result);
@@ -243,6 +233,7 @@ async function share() {
     if (result.error_code === 0) {
         console.log(`    分享: 成功 ,预计获得 ${result.data.credit} 爱豆`);
         msg += `\n    分享: 成功 ,预计获得 ${result.data.credit} 爱豆`;
+        await wait(3);
     } else {
         console.log(`    分享: 失败 ❌ 了呢,原因未知!`);
         console.log(result);
@@ -314,7 +305,7 @@ async function breed() {
     if (result.error_code === 0) {
         console.log(`    培育: 成功 `);
         msg += `\n    培育: 成功 `;
-        await $.wait(3 * 1000);
+        await wait(3);
         await breed();
     } else if (result.error_code === 1) {
         console.log(`    培育: ${result.error_message}`);
@@ -334,7 +325,7 @@ async function breed() {
 
 
 /**
- * 好友培育    POST
+ * 好友培育信息    POST
  */
 async function friend_breed_info() {
     let options = {
@@ -349,37 +340,32 @@ async function friend_breed_info() {
             "landId": lang_id,
         })
     };
-    let result = await httpRequest(options, `好友培育`);
+    let result = await httpRequest(options, `好友培育信息`);
 
     if (result.error_code === 0) {
         if (result.data.friends.length > 3) {
-            console.log(`    好友培育:您当前有 ${result.data.friends.length - 1} 个好友 ,将会进行好友培育任务!`);
-            msg += `\n    好友培育:您当前有 ${result.data.friends.length - 1} 个好友 ,将会进行好友培育任务!`;
+            console.log(`    好友培育信息:您当前有 ${result.data.friends.length - 1} 个好友 ,将会进行 好友培育 任务!`);
+            msg += `\n    好友培育信息:您当前有 ${result.data.friends.length - 1} 个好友 ,将会进行 好友培育 任务!`;
             for (let index = 1; index < 4; index++) {
                 let friend_id = result.data.friends[index].manor.user_id;
                 let friend_name = result.data.friends[index].manor.name;
                 let landId = result.data.friends[index].land.id;
-                console.log(`    好友培育:培育好友 ${friend_name} ,庄园id:${landId}`);
-                msg += `\n    好友培育:培育好友 ${friend_name} ,庄园id:${landId}`
+                console.log(`    好友培育信息:培育好友 ${friend_name} ,庄园id:${landId}`);
+                msg += `\n    好友培育信息:培育好友 ${friend_name} ,庄园id:${landId}`
                 await friend_breed(friend_id, landId);
-                await $.wait(3 * 1000);
             }
             await friend_breed_Reward();
-            await $.wait(3 * 1000);
-
-
         } else {
-            console.log(`    好友培育:您当前有 ${result.data.friends.length - 1} 个好友 ,跳过 好友培育任务!`);
-            msg += `\n    好友培育:您当前有 ${result.data.friends.length - 1} 个好友 ,将会进行 好友培育任务!`;
+            console.log(`    好友培育信息:您当前有 ${result.data.friends.length - 1} 个好友 ,跳过 好友培育 任务!`);
+            msg += `\n    好友培育信息:您当前有 ${result.data.friends.length - 1} 个好友 ,跳过 好友培育 任务!`;
         }
-
     } else if (result.error_code === 1) {
-        console.log(`    好友培育: ${result.error_message}`);
-        msg += `\n    好友培育: ${result.error_message}`;
+        console.log(`    好友培育信息: ${result.error_message}`);
+        msg += `\n    好友培育信息: ${result.error_message}`;
     } else {
-        console.log(`    好友培育: 失败 ❌ 了呢,原因未知!`);
+        console.log(`    好友培育信息: 失败 ❌ 了呢,原因未知!`);
         console.log(result);
-        msg += `\n    好友培育: 失败 ❌ 了呢,原因未知!`;
+        msg += `\n    好友培育信息: 失败 ❌ 了呢,原因未知!`;
     }
 }
 
@@ -407,6 +393,7 @@ async function friend_breed(friend_id, landId) {
     if (result.error_code === 0) {
         console.log(`    好友培育:成功了!`);
         msg += `\n    好友培育:成功了!`;
+        await wait(3);
     } else if (result.error_code === 1) {
         console.log(`    好友培育: ${result.error_message}`);
         msg += `\n    好友培育: ${result.error_message}`;
@@ -421,11 +408,12 @@ async function friend_breed(friend_id, landId) {
 
 /**
  * 领取好友培育奖励   POST
+ * https://coffeefarm.shheywow.com/api/user/taskv2/foster/friend/get/credit
  */
 async function friend_breed_Reward() {
     let options = {
         method: 'POST',
-        url: `${hostname}/user/taskv2/foster/friend/get/credit`,
+        url: `${hostname}/api/user/taskv2/foster/friend/get/credit`,
         headers: {
             'Host': host,
             'Authorization': `Bearer ${_tokne}`,
@@ -436,8 +424,9 @@ async function friend_breed_Reward() {
     let result = await httpRequest(options, `领取好友培育奖励`);
 
     if (result.error_code === 0) {
-        console.log(`    领取好友培育奖励:成功了`);
-        msg += `\n    领取好友培育奖励:成功了`;
+        console.log(`    领取好友培育奖励: 成功了`);
+        msg += `\n    领取好友培育奖励: 成功了`;
+        await wait(3);
     } else if (result.error_code === 1) {
         console.log(`    领取好友培育奖励: ${result.error_message}`);
         msg += `\n    领取好友培育奖励: ${result.error_message}`;
@@ -469,6 +458,7 @@ async function invitation_Reward() {
     if (result.error_code === 0) {
         console.log(`    领取邀请奖励:成功了`);
         msg += `\n    领取邀请奖励:成功了`;
+        await wait(3);
     } else if (result.error_code === 1) {
         console.log(`    领取邀请奖励: ${result.error_message}`);
         msg += `\n    领取邀请奖励: ${result.error_message}`;
@@ -498,6 +488,7 @@ async function share_Reward() {
     if (result.error_code === 0) {
         console.log(`    领取每日分享奖励:领取成功了! `);
         msg += `\n    领取每日分享奖励:领取成功了! `;
+        await wait(3);
     } else if (result.error_code === 1) {
         console.log(`    领取每日分享奖励: ${result.error_message}`);
         msg += `\n    领取每日分享奖励: ${result.error_message}`;
@@ -840,5 +831,5 @@ function debugLog(...args) {
 // 完整 Env
 function Env(t, e) { "undefined" != typeof process && JSON.stringify(process.env).indexOf("GITHUB") > -1 && process.exit(0); class s { constructor(t) { this.env = t } send(t, e = "GET") { t = "string" == typeof t ? { url: t } : t; let s = this.get; return "POST" === e && (s = this.post), new Promise((e, i) => { s.call(this, t, (t, s, r) => { t ? i(t) : e(s) }) }) } get(t) { return this.send.call(this.env, t) } post(t) { return this.send.call(this.env, t, "POST") } } return new class { constructor(t, e) { this.name = t, this.http = new s(this), this.data = null, this.dataFile = "box.dat", this.logs = [], this.isMute = !1, this.isNeedRewrite = !1, this.logSeparator = "\n", this.startTime = (new Date).getTime(), Object.assign(this, e), this.log("", `🔔${this.name}, 开始!`) } isNode() { return "undefined" != typeof module && !!module.exports } isQuanX() { return "undefined" != typeof $task } isSurge() { return "undefined" != typeof $httpClient && "undefined" == typeof $loon } isLoon() { return "undefined" != typeof $loon } toObj(t, e = null) { try { return JSON.parse(t) } catch { return e } } toStr(t, e = null) { try { return JSON.stringify(t) } catch { return e } } getjson(t, e) { let s = e; const i = this.getdata(t); if (i) try { s = JSON.parse(this.getdata(t)) } catch { } return s } setjson(t, e) { try { return this.setdata(JSON.stringify(t), e) } catch { return !1 } } getScript(t) { return new Promise(e => { this.get({ url: t }, (t, s, i) => e(i)) }) } runScript(t, e) { return new Promise(s => { let i = this.getdata("@chavy_boxjs_userCfgs.httpapi"); i = i ? i.replace(/\n/g, "").trim() : i; let r = this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout"); r = r ? 1 * r : 20, r = e && e.timeout ? e.timeout : r; const [o, h] = i.split("@"), n = { url: `http://${h}/v1/scripting/evaluate`, body: { script_text: t, mock_type: "cron", timeout: r }, headers: { "X-Key": o, Accept: "*/*" } }; this.post(n, (t, e, i) => s(i)) }).catch(t => this.logErr(t)) } loaddata() { if (!this.isNode()) return {}; { this.fs = this.fs ? this.fs : require("fs"), this.path = this.path ? this.path : require("path"); const t = this.path.resolve(this.dataFile), e = this.path.resolve(process.cwd(), this.dataFile), s = this.fs.existsSync(t), i = !s && this.fs.existsSync(e); if (!s && !i) return {}; { const i = s ? t : e; try { return JSON.parse(this.fs.readFileSync(i)) } catch (t) { return {} } } } } writedata() { if (this.isNode()) { this.fs = this.fs ? this.fs : require("fs"), this.path = this.path ? this.path : require("path"); const t = this.path.resolve(this.dataFile), e = this.path.resolve(process.cwd(), this.dataFile), s = this.fs.existsSync(t), i = !s && this.fs.existsSync(e), r = JSON.stringify(this.data); s ? this.fs.writeFileSync(t, r) : i ? this.fs.writeFileSync(e, r) : this.fs.writeFileSync(t, r) } } lodash_get(t, e, s) { const i = e.replace(/\[(\d+)\]/g, ".$1").split("."); let r = t; for (const t of i) if (r = Object(r)[t], void 0 === r) return s; return r } lodash_set(t, e, s) { return Object(t) !== t ? t : (Array.isArray(e) || (e = e.toString().match(/[^.[\]]+/g) || []), e.slice(0, -1).reduce((t, s, i) => Object(t[s]) === t[s] ? t[s] : t[s] = Math.abs(e[i + 1]) >> 0 == +e[i + 1] ? [] : {}, t)[e[e.length - 1]] = s, t) } getdata(t) { let e = this.getval(t); if (/^@/.test(t)) { const [, s, i] = /^@(.*?)\.(.*?)$/.exec(t), r = s ? this.getval(s) : ""; if (r) try { const t = JSON.parse(r); e = t ? this.lodash_get(t, i, "") : e } catch (t) { e = "" } } return e } setdata(t, e) { let s = !1; if (/^@/.test(e)) { const [, i, r] = /^@(.*?)\.(.*?)$/.exec(e), o = this.getval(i), h = i ? "null" === o ? null : o || "{}" : "{}"; try { const e = JSON.parse(h); this.lodash_set(e, r, t), s = this.setval(JSON.stringify(e), i) } catch (e) { const o = {}; this.lodash_set(o, r, t), s = this.setval(JSON.stringify(o), i) } } else s = this.setval(t, e); return s } getval(t) { return this.isSurge() || this.isLoon() ? $persistentStore.read(t) : this.isQuanX() ? $prefs.valueForKey(t) : this.isNode() ? (this.data = this.loaddata(), this.data[t]) : this.data && this.data[t] || null } setval(t, e) { return this.isSurge() || this.isLoon() ? $persistentStore.write(t, e) : this.isQuanX() ? $prefs.setValueForKey(t, e) : this.isNode() ? (this.data = this.loaddata(), this.data[e] = t, this.writedata(), !0) : this.data && this.data[e] || null } initGotEnv(t) { this.got = this.got ? this.got : require("got"), this.cktough = this.cktough ? this.cktough : require("tough-cookie"), this.ckjar = this.ckjar ? this.ckjar : new this.cktough.CookieJar, t && (t.headers = t.headers ? t.headers : {}, void 0 === t.headers.Cookie && void 0 === t.cookieJar && (t.cookieJar = this.ckjar)) } get(t, e = (() => { })) { t.headers && (delete t.headers["Content-Type"], delete t.headers["Content-Length"]), this.isSurge() || this.isLoon() ? (this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, { "X-Surge-Skip-Scripting": !1 })), $httpClient.get(t, (t, s, i) => { !t && s && (s.body = i, s.statusCode = s.status), e(t, s, i) })) : this.isQuanX() ? (this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, { hints: !1 })), $task.fetch(t).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => e(t))) : this.isNode() && (this.initGotEnv(t), this.got(t).on("redirect", (t, e) => { try { if (t.headers["set-cookie"]) { const s = t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString(); s && this.ckjar.setCookieSync(s, null), e.cookieJar = this.ckjar } } catch (t) { this.logErr(t) } }).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => { const { message: s, response: i } = t; e(s, i, i && i.body) })) } post(t, e = (() => { })) { if (t.body && t.headers && !t.headers["Content-Type"] && (t.headers["Content-Type"] = "application/x-www-form-urlencoded"), t.headers && delete t.headers["Content-Length"], this.isSurge() || this.isLoon()) this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, { "X-Surge-Skip-Scripting": !1 })), $httpClient.post(t, (t, s, i) => { !t && s && (s.body = i, s.statusCode = s.status), e(t, s, i) }); else if (this.isQuanX()) t.method = "POST", this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, { hints: !1 })), $task.fetch(t).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => e(t)); else if (this.isNode()) { this.initGotEnv(t); const { url: s, ...i } = t; this.got.post(s, i).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => { const { message: s, response: i } = t; e(s, i, i && i.body) }) } } time(t, e = null) { const s = e ? new Date(e) : new Date; let i = { "M+": s.getMonth() + 1, "d+": s.getDate(), "H+": s.getHours(), "m+": s.getMinutes(), "s+": s.getSeconds(), "q+": Math.floor((s.getMonth() + 3) / 3), S: s.getMilliseconds() }; /(y+)/.test(t) && (t = t.replace(RegExp.$1, (s.getFullYear() + "").substr(4 - RegExp.$1.length))); for (let e in i) new RegExp("(" + e + ")").test(t) && (t = t.replace(RegExp.$1, 1 == RegExp.$1.length ? i[e] : ("00" + i[e]).substr(("" + i[e]).length))); return t } msg(e = t, s = "", i = "", r) { const o = t => { if (!t) return t; if ("string" == typeof t) return this.isLoon() ? t : this.isQuanX() ? { "open-url": t } : this.isSurge() ? { url: t } : void 0; if ("object" == typeof t) { if (this.isLoon()) { let e = t.openUrl || t.url || t["open-url"], s = t.mediaUrl || t["media-url"]; return { openUrl: e, mediaUrl: s } } if (this.isQuanX()) { let e = t["open-url"] || t.url || t.openUrl, s = t["media-url"] || t.mediaUrl; return { "open-url": e, "media-url": s } } if (this.isSurge()) { let e = t.url || t.openUrl || t["open-url"]; return { url: e } } } }; if (this.isMute || (this.isSurge() || this.isLoon() ? $notification.post(e, s, i, o(r)) : this.isQuanX() && $notify(e, s, i, o(r))), !this.isMuteLog) { let t = ["", "==============📣系统通知📣=============="]; t.push(e), s && t.push(s), i && t.push(i), console.log(t.join("\n")), this.logs = this.logs.concat(t) } } log(...t) { t.length > 0 && (this.logs = [...this.logs, ...t]), console.log(t.join(this.logSeparator)) } logErr(t, e) { const s = !this.isSurge() && !this.isQuanX() && !this.isLoon(); s ? this.log("", `❗️${this.name}, 错误!`, t.stack) : this.log("", `❗️${this.name}, 错误!`, t) } wait(t) { return new Promise(e => setTimeout(e, t)) } done(t = {}) { const e = (new Date).getTime(), s = (e - this.startTime) / 1e3; this.log("", `🔔${this.name}, 结束! 🕛 ${s} 秒`), this.log(), (this.isSurge() || this.isQuanX() || this.isLoon()) && $done(t) } }(t, e) }
 
- //#endregion
+   //#endregion
 
