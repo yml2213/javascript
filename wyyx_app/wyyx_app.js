170ebef412a3d8a660fcacc33998b622ae6edaf6
yml2213 <a2213290201@gmail.com>
2022-06-12 20:40:58 +0800

    修复bug3

--- wyyx_app/wyyx_app.js
+++ wyyx_app/wyyx_app.js
@@ -6,7 +6,7 @@
  *
  * cron 20 7,12  * * *  yml2213_javascript_master/wyyx_app.js
  *
- * 6-12		完成 签到 任务 (重写应该可以了)
+ * 6-12		完成 签到 任务 (重写应该是不行 ,折腾了好久  自己抓包 boxjs添加数据吧)
  *
  * 感谢所有测试人员
  * ========= 青龙--配置文件 =========
@@ -36,14 +36,14 @@ let ck_status = true;
 let host = 'act.you.163.com';
 let hostname = 'https://' + host;
 //---------------------------------------------------------------------------------------------------------
-let VersionCheck = "0.0.2"
+let VersionCheck = "0.1.2"
 let Change = '增加圈x v2p兼容,自行测试吧!'
 let thank = `\n感谢 心雨 的投稿\n`
 //---------------------------------------------------------------------------------------------------------
 
 async function tips(ckArr) {
 	let Version_latest = await Version_Check('wyyx_app');
-	let Version = `\n📌 本地脚本: V 0.0.2  远程仓库脚本: V ${Version_latest}`
+	let Version = `\n📌 本地脚本: V 0.1.2  远程仓库脚本: V ${Version_latest}`
 	DoubleLog(`${Version}\n📌 🆙 更新内容: ${Change}`);
 	// DoubleLog(`${thank}`);
 	await wyy();
@@ -89,6 +89,7 @@ async function start() {
 		await point_info(2);
 
 	}
+
 }
 
 
@@ -97,7 +98,8 @@ async function start() {
 // https://act.you.163.com/act-attendance/task/list
 async function GetRewrite() {
 	if ($request.url.indexOf("act-attendance/task/list") > -1) {
-		ck = $request.headers.Cookie;
+		const ck = $request.headers.cookie;
+		console.log(ck);
 		if (ckStr) {
 			if (ckStr.indexOf(ck) == -1) {  // 找不到返回 -1
 				ckStr = ckStr + "@@" + ck;
@@ -234,26 +236,22 @@ async function task_list() {
 /**
  *
  * 签到    httpGet
- * https://mc.kukahome.com/club-server/front/member/signIn
+ * https://act.you.163.com/act-attendance/att/v3/sign
  */
 async function signIn() {
 	let Options = {
-		url: `${hostname}/club-server/front/member/signIn`,
+		url: `${hostname}/act-attendance/att/v3/sign`,
 		headers: {
 			'Host': host,
-			'X-Customer': ck[0],
-			'brandCode': 'K001',
-			'content-type': 'application/json',
+			'Cookie': ck[0]
 		},
-		body: JSON.stringify({ "identityType": "mobile", "membershipId": ck[0], "customerId": customerId, "authorization": ck[1] })
 	};
 	let result = await httpGet(Options, `签到`);
 
-	console.log(result);
 	if (result.code == 200) {
 		DoubleLog(`签到: 成功 🎉`);
-	} else if (result.code == 400) {
-		DoubleLog(`签到信息: ${result.message}`);
+	} else if (result.code == 1009) {
+		DoubleLog(`签到信息: ${result.msg}`);
 	} else {
 		DoubleLog(`签到: 失败 ❌ 了呢,原因未知!`);
 		console.log(result);
