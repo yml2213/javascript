9d11abedc3d137e30df36f85819253341c829a2f
yml2213 <a2213290201@gmail.com>
2022-06-12 13:57:22 +0800

    修复bug 1

--- gjjjhyjlb/gjjjhyjlb.js
+++ gjjjhyjlb/gjjjhyjlb.js
@@ -26,7 +26,7 @@
 const $ = new Env("顾家家居会员俱乐部");
 const notify = $.isNode() ? require("./sendNotify") : "";
 const Notify = 1 		//0为关闭通知,1为打开通知,默认为1
-const debug = 1			//0为关闭调试,1为打开调试,默认为0
+const debug = 0			//0为关闭调试,1为打开调试,默认为0
 //---------------------------------------------------------------------------------------------------------
 let ckStr = ($.isNode() ? process.env.gjjjhyjlb_data : $.getdata('gjjjhyjlb_data')) || '';
 let msg, ck, ck_status;
