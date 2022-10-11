0bffb5707b1424dbd7fa960967cacf3792f38129
yml2213 <a2213290201@gmail.com>
2022-10-11 21:22:02 +0800

    修复错误

--- sbr/sbr.js
+++ sbr/sbr.js
@@ -58,7 +58,7 @@ async function start() {
 	}
 }
 
-let host, hostname, apiname, sbr_hd;
+let host, hostname, apiname, sbr_hd, _id;
 class Sbr {
 	constructor(token, cookie) {
 		this.token = token;
