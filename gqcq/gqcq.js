695895bf91df1648491a2d0d9f9c23918e72c6e9
yml2213 <a2213290201@gmail.com>
2023-01-03 20:17:52 +0800

    更新分隔符

--- gqcq/gqcq.js
+++ gqcq/gqcq.js
@@ -36,7 +36,7 @@ const ckName = "gqcq"
 //-------------------- 一般不动变量区域 -------------------------------------
 const notify = $.isNode() ? require("./sendNotify") : ""
 const Notify = 1		 //0为关闭通知,1为打开通知,默认为1
-let envSplitor = ["@", "\n"]
+let envSplitor = ["@", "\n", "&"]
 let ck = msg = ''
 let host, hostname
 let userCookie = process.env[ckName]
